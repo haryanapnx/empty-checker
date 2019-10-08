8f14e61c307b1b61d05fba870bfe4100ff61f92c
unknown <bnugraha00@gmail.com>
2019-10-08 12:51:18 +0700

    trim string

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const isEmpty = function (value) {
+   value = typeof value == 'string' ? value.trim() : value
    if (!value || value === '0') { return true }
    if (typeof value === 'object') {
       for (var k in value) {
