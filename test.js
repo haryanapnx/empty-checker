7f39e4e3fa66dc2a21fd6b47dec20413f8bd0181
backend <engineer@backends-MacBook-Pro.local>
2019-10-08 12:44:34 +0700

    add testing

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
 const isEmpty = require('./index')
 const assert = require('assert')
+
 // true when value = 0
 assert.equal(true, isEmpty(0), '0 is empty');
-
 // true when value = ""
 assert.equal(true, isEmpty(""), '"" is empty');
-
 // false when value = "1"
 assert.equal(false, isEmpty(1), '1 is not empty');
-
 // true when value = "0"
 assert.equal(true, isEmpty("0"), '0 is empty');
-
-// true when value = "0"
-// console.log(emptyObject)
+// true when value  = {}
 return assert.equal(true, isEmpty({}), '{} is empty');
