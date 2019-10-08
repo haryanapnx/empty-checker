239eea91d3b097f031365d71f39c06570a5104f2
backend <engineer@backends-MacBook-Pro.local>
2019-10-08 12:37:40 +0700

    empty object

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const isEmpty = function (value) {
-   return (!value || value == "0" || 0 === value.length);
+   return (!value || value == "0" || 0 === value.length || Object.entries(value).length === 0 && value.constructor === Object);
 }
 module.exports = isEmpty
--- test.js
+++ test.js
@@ -7,7 +7,11 @@ assert.equal(true, isEmpty(0), '0 is empty');
 assert.equal(true, isEmpty(""), '"" is empty');
 
 // false when value = "1"
-assert.equal(false, isEmpty(1), '1 is note empty');
+assert.equal(false, isEmpty(1), '1 is not empty');
 
 // true when value = "0"
 assert.equal(true, isEmpty("0"), '0 is empty');
+
+// true when value = "0"
+// console.log(emptyObject)
+return assert.equal(true, isEmpty({}), '{} is empty');
