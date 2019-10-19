550021a76cb270a873d97470bcfb8971d488e2c7
Michael <michael.wenceslaus@gmail.com>
2019-10-19 12:42:12 +0700

    remove white space and add test for value that containing white space

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 /**
  * Author: https://github.com/haryanapnx
- * @param {*} value 
+ * @param {*} value
  * @returns {boolean}
  */
 
 /**
  * This function will checking for value that
  * passed into parameter, and check the value
- * 
+ *
  * if value {null}, {undefined}, {0}, {''}
  * will returns true
  */
 const isEmpty = function (value) {
-   return (!value || value == "0" || 0 === value.length || Object.entries(value).length === 0 && value.constructor === Object);
-}
-module.exports = isEmpty
+   var remSpace = value.toString().replace(/\s/gi, "");
+   return (
+       !value ||
+       value == "0" ||
+       0 === value.length ||
+       Object.entries(value).length === 0 &&
+       value.constructor === Object ||
+       remSpace === ""
+   );
+};
+module.exports = isEmpty;
--- test.js
+++ test.js
@@ -11,3 +11,7 @@ assert.equal(false, isEmpty(1), '1 is not empty');
 assert.equal(true, isEmpty("0"), '0 is empty');
 // true when value  = {}
 assert.equal(true, isEmpty({}), '{} is empty');
+// true when value  = " "
+assert.equal(true, isEmpty(" "), '" " is empty');
+// false when value  = " a s d  "
+assert.equal(false, isEmpty(" a s d "), '" a s d " is not empty');
