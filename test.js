f4c732b12dec16dd3b3bab7a043cb36daa319f1a
Michael <michael.wenceslaus@gmail.com>
2019-10-20 18:26:15 +0700

    change equal to => strictEqual (assert.equal() is deprecated since node v10) https://github.com/googleapis/gapic-generator/issues/2154

--- test.js
+++ test.js
@@ -2,16 +2,18 @@ const isEmpty = require('./index')
 const assert = require('assert')
 
 // true when value = 0
-assert.equal(true, isEmpty(0), '0 is empty');
+assert.strictEqual(true, isEmpty(0), '0 is empty');
 // true when value = ""
-assert.equal(true, isEmpty(""), '"" is empty');
+assert.strictEqual(true, isEmpty(""), '"" is empty');
 // false when value = "1"
-assert.equal(false, isEmpty(1), '1 is not empty');
+assert.strictEqual(false, isEmpty(1), '1 is not empty');
 // true when value = "0"
-assert.equal(true, isEmpty("0"), '0 is empty');
+assert.strictEqual(true, isEmpty("0"), '0 is empty');
 // true when value  = {}
-assert.equal(true, isEmpty({}), '{} is empty');
+assert.strictEqual(true, isEmpty({}), '{} is empty');
 // true when value  = " "
-assert.equal(true, isEmpty(" "), '" " is empty');
+assert.strictEqual(true, isEmpty(" "), '" " is empty');
 // false when value  = " a s d  "
-assert.equal(false, isEmpty(" a s d "), '" a s d " is not empty');
+assert.strictEqual(false, isEmpty(" a s d "), '" a s d " is not empty');
+
+console.log("test running successfully");
